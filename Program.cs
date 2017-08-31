2e48e42d474b300174a308d4a483cb420630d0a0
Quang Nguyen <quang.bme.hust.55@gmail.com>
2017-08-31 16:30:29 +0900

    Test deserialize

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace JSONParser
 {
   class Program
   {
+    static string TEST_JSON = "{\"FirstName\":\"Quang\",\"LastName\":\"Nguyen\",\"Birthday\":\"1992-02-03T00:00:00\",\"Grades\":[8,9,8]}";
     static void Main(string[] args)
     {
       testJSON();
@@ -18,16 +20,21 @@ namespace JSONParser
       student.Birthday = new DateTime(1992, 02, 03);
       student.Grades = new int[] { 8, 9, 8 };
 
-      Console.WriteLine("JSON serialziation");
+      Console.WriteLine("JSON serialziation:");
       Console.WriteLine(encodeStudent(student));
+
+      Console.WriteLine("JSON deserialization:");
+      Console.WriteLine(decodeStudent(TEST_JSON));
     }
 
-    static string encodeStudent(Student student) {
+    static string encodeStudent(Student student)
+    {
       return JsonConvert.SerializeObject(student);
     }
 
-    static Student decodeStudent(String json){
-      Student student = new Student();
+    static Student decodeStudent(String json)
+    {
+      Student student = JObject.Parse(json).ToObject<Student>();
       return student;
     }
   }
--- Student.cs
+++ Student.cs
@@ -7,5 +7,10 @@ namespace JSONParser
     public string LastName { get; set; }
     public DateTime Birthday { get; set; }
     public int[] Grades { get; set; }
+
+    public override string ToString()
+    {
+      return string.Format("[Student: FirstName={0}, LastName={1}, Birthday={2}, Grades={3}]", FirstName, LastName, Birthday, string.Join(",", Grades));
+    }
   }
 }
