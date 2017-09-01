b529e11c28bdaf8d177db2b1f9dcf61ec7d6a554
Quang Nguyen <quang.bme.hust.55@gmail.com>
2017-09-01 11:30:11 +0900

    Miscellaneous.

---
+++ JsonHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace JSONParser
+{
+  public class JsonHandler
+  {
+    public String Encode(Student student) {
+      return JsonConvert.SerializeObject(student);
+    }
+    public Student Decode(String json) {
+      return JObject.Parse(json).ToObject<Student>();
+    }
+  }
+}
---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace JSONParser
+{
+  public class Student
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime Birthday { get; set; }
+    public int[] Grades { get; set; }
+
+    public override string ToString()
+    {
+      return string.Format("[Student: FirstName={0}, LastName={1}, Birthday={2}, Grades={3}]",
+                           FirstName,
+                           LastName,
+                           Birthday,
+                           string.Join(",", Grades));
+    }
+
+    public override bool Equals(object obj)
+    {
+      if (!(obj is Student)) {
+        return false;
+      } else {
+        Student student = (Student)obj;
+        return this.ToString().Equals(student.ToString());
+      }
+    }
+  }
+}
